81020bfc5f6a570d6500948688288a64c3354008
Gabriel Bento <gabriel_holanda@hotmail.com.br>
2024-09-03 09:40:28 -0400

    Initial git commit

---
+++ src/lib.rs
@@ -0,0 +1,39 @@
+use std::io::{self, Write};
+
+static mut HAD_ERROR: bool = false;
+
+// TODO: In page 42 there's a place to check runtime error (a.k.a HAD_ERROR)
+// I haven't yet done this due that functions are still in process making.
+
+pub fn error(line: usize, message: &str) {
+    report(line, "", message);
+}
+
+pub fn report(line: usize, location: &str, message: &str) {
+    eprintln!("[Line {}] Error {}: {}", line, location, message);
+    // Aww men... Here goes unsafe :( Is there another way to make this?
+    unsafe { HAD_ERROR = true };
+}
+
+pub fn run_file(path: &str) {}
+
+// REPL mode
+pub fn run_prompt() {
+    loop {
+        print!(">> ");
+        let mut user_input = String::new();
+        let _ = io::stdout().flush();
+        let bytes = io::stdin().read_line(&mut user_input).unwrap();
+
+        let user_input = user_input.trim();
+        if user_input == "exit" || bytes == 0 {
+            break;
+        }
+
+        run(user_input);
+    }
+}
+
+fn run(source: &str) {
+
+}
---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use std::{env, process};
+
+use rlox::{run_file, run_prompt};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    // for i in 0..args.len() {
+    //     println!("Arg {}: {}", i, args[i]);
+    // }
+
+    match args.len() {
+        // No arguments passed. Shows REPL.
+        1 => run_prompt(),
+        // '.lox' file passed. Runs file's source code.
+        2 => run_file(args.get(1).unwrap().as_str()),
+        // Bad usage. Shows message.
+        _ => {
+            println!("Usage: jlox [script");
+            process::exit(64)
+        }
+    }
+}
