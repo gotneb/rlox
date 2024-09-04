c51324938d455c072731daf98cee8093cb0f03c0
Gabriel Bento <gabriel_holanda@hotmail.com.br>
2024-09-04 10:51:12 -0400

    feat: scanner scans identifies
    Also, moved method `string` location for better code reading.

--- src/scanner.rs
+++ src/scanner.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use crate::{
     error, token::{Literal, Token}, token_type::TokenType
 };
@@ -90,10 +92,24 @@ impl Scanner {
                 // verify other cases instead of numbers
                 if self.is_digit(c) {
                     self.number()
+                } else if self.is_alpha(c) {
+                    self.identifier()
                 } else {
                     error(self.line, "Unexpected character.");
                 }
             },
+        }
+    }
+
+    fn identifier(&mut self) {
+        while self.is_alpha_numeric(self.peek()) {
+            self.advance();
+        }
+
+        let text = self.source[self.start..self.current].to_string();
+        match self.get_keywords().get(&text) {
+            Some(token_type) => self.add_token(token_type.clone(), Literal::None),
+            None => self.add_token(TokenType::Identifier, Literal::None),
         }
     }
 
@@ -117,6 +133,29 @@ impl Scanner {
                             .parse()
                             .unwrap();
         self.add_token(TokenType::Number, Literal::Number(value))
+    }
+
+    fn string(&mut self) {
+        if self.peek() != '"' && !self.is_at_end() {
+            // Lox has support for multi-string
+            if self.peek() == '\n' {
+                self.line += 1;
+            }
+            self.advance();
+        }
+
+        if self.is_at_end() {
+            return error(self.line, "Unterminated string");
+        }
+
+        // The closing ".
+        self.advance();
+
+        // Trim surrounding quotes
+        let value = self.source[self.start+1..self.current-1]
+                            .to_string();
+        self.add_token(TokenType::String, Literal::String(value));
+
     }
 
     fn add_token(&mut self, token_type: TokenType, literal: Literal) {
@@ -164,30 +203,42 @@ impl Scanner {
         self.source.as_bytes()[self.current + 1] as char
     }
 
+    fn is_alpha(&self, c: char) -> bool {
+        return (c >= 'a' && c <= 'z') ||
+               (c >= 'A' && c <= 'Z') || 
+               c == '_';
+    }
+
+    // TODO: Rust' std has a lib for this, i think...
+    fn is_alpha_numeric(&self, c: char) -> bool {
+        return self.is_alpha(c) || self.is_digit(c)
+    }
+
+    // TODO: Rust' std has a lib for this, i think...
     fn is_digit(&self, c: char) -> bool {
         return c >= '0' && c <= '9'
     }
 
-    fn string(&mut self) {
-        if self.peek() != '"' && !self.is_at_end() {
-            // Lox has support for multi-string
-            if self.peek() == '\n' {
-                self.line += 1;
-            }
-            self.advance();
-        }
+    fn get_keywords(&self) -> HashMap<String, TokenType> {
+        let mut hash = HashMap::new();
+        
+        hash.insert("and".into(), TokenType::And);
+        hash.insert("class".into(), TokenType::Class);
+        hash.insert("else".into(), TokenType::Else);
+        hash.insert("false".into(), TokenType::False);
+        hash.insert("for".into(), TokenType::For);
+        hash.insert("fun".into(), TokenType::Fun);
+        hash.insert("if".into(), TokenType::If);
+        hash.insert("nil".into(), TokenType::Nil);
+        hash.insert("or".into(), TokenType::Or);
+        hash.insert("print".into(), TokenType::Print);
+        hash.insert("return".into(), TokenType::Return);
+        hash.insert("super".into(), TokenType::Super);
+        hash.insert("this".into(), TokenType::This);
+        hash.insert("true".into(), TokenType::True);
+        hash.insert("var".into(), TokenType::Var);
+        hash.insert("while".into(), TokenType::While);
 
-        if self.is_at_end() {
-            return error(self.line, "Unterminated string");
-        }
-
-        // The closing ".
-        self.advance();
-
-        // Trim surrounding quotes
-        let value = self.source[self.start+1..self.current-1]
-                            .to_string();
-        self.add_token(TokenType::String, Literal::String(value));
-
+        hash
     }
 }
